416b6aa9af799dc4fc5982ed27cef9b08548b377
Nguyen Minh Tam <nminhtam425@gmail.com>
2026-02-05 09:37:22 +0700

    update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const tableStudents = document.querySelector('#tableStudents')
 let urlAvatar = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr0R0KswnhNi-1bsU96g79bF4IafZtcrAVzg&s"
 let urlApi = "https://698300279c3efeb892a408bf.mockapi.io/crud-student/students/"
 let idStudents = -1
+let elementForUpdate = null
 function updateItem(){
     
 }
@@ -22,54 +23,56 @@ function createRow(data){
     row.appendChild(arrCell[0])
 
     let td1 = document.createElement('td')
-    td1.innerHTML = `<span class="btn bg-danger" value=${data.id}>Delete</span>`
-    td1.innerHTML += `<span class="btn bg-warning" value=${data.id}>Edit</span>`
+
+    let btnDelete = document.createElement('button')
+    btnDelete.classList.add('btn','bg-danger')
+    btnDelete.innerText = 'Delete'
+
+    let btnUpdate = document.createElement('button')
+    btnUpdate.classList.add('btn', 'bg-warning')
+    btnUpdate.innerText = 'Edit'
+
+    btnDelete.value = data.id
+    btnUpdate.value = data.id
+
+    btnDelete.onclick = (e) => {
+        fetch(`https://698300279c3efeb892a408bf.mockapi.io/crud-student/students/${data.id}`,{
+            method:"DELETE"
+        })
+        .then(res => res.json())
+        .then(student => {
+            if(student.id == data.id){
+                row.remove()
+            }
+        })
+    }
+
+    btnUpdate.onclick = (e) => {
+        idStudents = data.id
+        text.value = data.name
+        elementForUpdate = arrCell[0]
+        text.focus()
+    }
+
+    td1.appendChild(btnDelete)
+    td1.appendChild(btnUpdate)
     row.appendChild(td1)
     return row
 }
+
 function students(){
     fetch("https://698300279c3efeb892a408bf.mockapi.io/crud-student/students")
     .then(res => res.json())
     .then(data => {
         for(let i of data)
             tableStudents.appendChild(createRow(i))
-                
-        let btnDelete = document.querySelectorAll('.bg-danger')
-        let btnUpdate = document.querySelectorAll('.bg-warning')
-
-        btnDelete.forEach(item => {
-            let id = item.getAttribute('value')
-            item.onclick = (e) => {
-                const row = e.target.closest('tr')
-                fetch(`https://698300279c3efeb892a408bf.mockapi.io/crud-student/students/${id}`,{
-                    method:"DELETE"
-                })
-                .then(res => res.json())
-                .then(student => {
-                    if(student.id == id){
-                        row.remove()
-                    }
-                })
-            }
-        })
-
-        btnUpdate.forEach(item => {
-            item.onclick = () => {
-                idStudents = Number(item.getAttribute('value'))
-                for(let i of data){
-                    if(i.id == idStudents)
-                        text.value = i.name
-                }
-                text.focus()
-            }
-        })
-        
     })
     .catch(err => {
         console.log(err)
     })
 }
 students()
+
 const headerTable = "<tr><th>ID</th><th>Avatar</th><th>Full Name</th><th>Action</th></tr>"
 let text = document.querySelector('#username')
 let btnSave = document.querySelector('#btnSave')
@@ -89,10 +92,10 @@ btnSave.onclick = (e) => {
             })
             .then(res => res.json())
             .then(student => {
-                console.log(student)
                 if(student.name == name){
                     tableStudents.appendChild(createRow(student))
                     text.value = ""
+                    alert("Thêm thành công !")
                 }
                 else{
                     fetch(`https://698300279c3efeb892a408bf.mockapi.io/crud-student/students/${student.id}`,{
@@ -116,9 +119,8 @@ btnSave.onclick = (e) => {
                 if(student.name == name){
                     text.value = ""
                     idStudents = -1
-                    tableStudents.innerHTML = ""
-                    tableStudents.innerHTML = headerTable
-                    students()
+                    elementForUpdate.innerText = student.name
+                    alert("Cập nhật thành công !")
                 }
                 else{
                     alert("Cập nhật thất bại !")
